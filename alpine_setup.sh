aa363ae331d7aafec407ad2dbba3f744594107d5
Emeka Ugwuanyi <emylincon@gmail.com>
2020-06-17 22:10:26 +0100

    improved

---
+++ alpine_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+apk update && apk upgrade
+apk add nano, curl, wget
+pip3 install pickle
+pip3 install mlxtend
+pip3 install pandas
+pip3 install paramiko
+pip3 install paho-mqtt
+pip3 install requests
---
+++ ubuntu_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt update && apt upgrade -y
+apt install python3-pip
+apt install nano, curl, wget
+pip3 install pickle
+pip3 install mlxtend
+pip3 install pandas
+pip3 install paramiko
+pip3 install paho-mqtt
+pip3 install requests
