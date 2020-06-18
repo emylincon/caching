a2b90cb3421fe85c921847e70257a8145ac75b7e
Emeka Ugwuanyi <emylincon@gmail.com>
2020-06-18 10:40:34 +0100

    improved

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3,7 +3,7 @@
 clear
 echo '============ Preparing your MEC Platform =============='
 sleep 3
-echo -e "\nupdating repository...\n"
+echo "\nupdating repository...\n"
 apt update && apt upgrade -y
 
 echo -e "installing python3...\n"
@@ -11,7 +11,7 @@ apt install python3 -y
 apt install python3-pip -y
 
 echo "installing required python3 packages..."
-echo -e "(drawnow, psutil, matplotlib, paramiko, pyfiglet, numpy)\n"
+echo "(drawnow, psutil, matplotlib, paramiko, pyfiglet, numpy)\n"
 pip3 install drawnow
 apt install python3-psutil -y
 apt install python3-matplotlib -y
@@ -24,12 +24,12 @@ pip3 install mlxtend
 pip3 install pandas
 pip3 install requests
 
-echo -e "installing openssh...\n"
+echo "installing openssh...\n"
 apt install openssh-client -y
 apt install openssh-server -y
 
 echo "installing network tools..."
-echo -e "(wget, apt-utils, iputils-ping, net-tools, nmap)\n"
+echo "(wget, apt-utils, iputils-ping, net-tools, nmap)\n"
 apt install wget -y
 apt install apt-utils -y
 apt install iputils-ping -y
@@ -37,7 +37,7 @@ apt install net-tools -y
 apt install nano -y
 apt install nmap -y
 clear
-echo -e "starting ssh server..\n"
+echo "starting ssh server..\n"
 sleep 1.5
 /etc/init.d/ssh start
 echo '============= All done.. Ready to use! ==============='
