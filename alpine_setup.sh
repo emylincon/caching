ec63b30d64bde28e33599bf9f37989f9cae44d2d
Emeka Ugwuanyi <emylincon@gmail.com>
2020-07-10 13:56:17 +0100

    installing scipy on alpine

--- alpine_setup.sh
+++ alpine_setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 apk update && apk upgrade
+echo "http://dl-8.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
+apk --no-cache --update-cache add gcc gfortran python python-dev py-pip build-base wget freetype-dev libpng-dev openblas-dev
+ln -s /usr/include/locale.h /usr/include/xlocale.h
+pip3 install numpy scipy pandas matplotlib
 pip3 install mlxtend
-pip3 install pandas
 pip3 install paramiko
 pip3 install paho-mqtt
 pip3 install requests
