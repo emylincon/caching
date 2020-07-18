c74efbf00e75a65c08cce032897fddfe9a10ba0b
Emeka Ugwuanyi <emylincon@gmail.com>
2020-07-18 23:02:28 +0100

    fp-growth algorithm

--- test_files/association_fpgrowths.py
+++ test_files/association_fpgrowths.py
@@ -3,14 +3,14 @@ import pandas as pd
 import pyfpgrowth
 
 transactions = [[1, 2, 5],
- [2, 4],
- [2, 3],
- [1, 2, 4],
-[1, 3],
-[2, 3],
-[1, 3],
-[1, 2,3, 5],
-[1, 2, 3]]
+                [2, 4],
+                [2, 3],
+                [1, 2, 4],
+                [1, 3],
+                [2, 3],
+                [1, 3],
+                [1, 2, 3, 5],
+                [1, 2, 3]]
 
 # Definition : find_frequent_patterns(transactions, support_threshold)
 patterns = pyfpgrowth.find_frequent_patterns(transactions, 2)
@@ -23,6 +23,5 @@ Given a set of frequent itemsets, return a dict of association rules in the form
 
 '''
 rules = pyfpgrowth.generate_association_rules(patterns, 0.7)
-
 
 print(rules)
