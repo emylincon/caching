5ddbef428468a8ea6b1952ca75b00c1d47701d59
Emeka Ugwuanyi <emylincon@gmail.com>
2020-07-23 13:33:50 +0100

    update

--- alpine_setup.sh
+++ alpine_setup.sh
@@ -2,7 +2,7 @@
 
 apk update && apk upgrade
 echo "http://dl-8.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
-apk --no-cache --update-cache add gcc gfortran python python-dev py-pip build-base wget freetype-dev libpng-dev openblas-dev
+apk --no-cache --update-cache add gcc gfortran python3-dev py-pip build-base wget freetype-dev libpng-dev openblas-dev
 ln -s /usr/include/locale.h /usr/include/xlocale.h
 pip3 install numpy scipy pandas matplotlib
 pip3 install mlxtend
