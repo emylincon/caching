230c5b48103565c717771e732b3482d1db51a4f6
Emeka Ugwuanyi <emylincon@gmail.com>
2020-07-27 09:30:24 +0100

    ready for testing..

--- LSTM_caching/predictive_caching.py
+++ LSTM_caching/predictive_caching.py
@@ -1,9 +1,7 @@
 import numpy as np
-import random as r
 import pandas as pd
 import pickle
 from sklearn.preprocessing import MinMaxScaler
-import requests
 import os
 import psutil
 import wget
@@ -14,6 +12,7 @@ import re
 import argparse
 import smtplib
 import config
+import paho.mqtt.client as mqtt
 
 trained = [1, 10, 100, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 101, 1010, 1011, 1012, 1013, 1014, 1015, 1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023, 1024, 1025, 1027, 1028, 1029, 1030, 1031, 1032, 1033, 1034, 1035, 1036, 1037, 1038, 104, 1040, 1041, 1042, 1043, 1044, 1046, 1047, 1049, 105, 1050, 1051, 1053, 1054, 1055, 1056, 1057, 1058, 1059, 1060, 107, 11, 110, 111, 112, 122, 125, 135, 14, 140, 141, 144, 145, 147, 150, 151, 153, 154, 155, 156, 157, 158, 159, 16, 160, 161, 162, 163, 164, 165, 166, 168, 169, 17, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 18, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 19, 190, 191, 193, 194, 195, 196, 198, 199, 2, 200, 204, 208, 21, 216, 22, 223, 224, 225, 230, 231, 235, 236, 24, 246, 247, 25, 252, 253, 256, 260, 261, 265, 266, 272, 277, 282, 288, 29, 292, 293, 296, 3, 300, 31, 315, 316, 317, 318, 32, 329, 333, 337, 339, 34, 342, 344, 345, 348, 349, 350, 353, 355, 356, 357, 36, 364, 367, 368, 370, 372, 376, 377, 380, 39, 41, 410, 420, 431, 432, 434, 435, 44, 440, 441, 442, 45, 454, 455, 457, 466, 47, 471, 474, 475, 48, 480, 485, 49272, 494, 497, 5, 50, 500, 508, 509, 515, 52, 520, 524, 527, 529, 53, 539, 54, 541, 543, 55, 551, 552, 553, 555, 57, 58, 585, 586, 587, 588, 589, 59, 590, 592, 593, 594, 595, 596, 597, 6, 60, 605, 608, 609, 61, 610, 613, 616, 62, 628, 63, 637, 6377, 64, 640, 647, 648, 65, 653, 66, 661, 662, 663, 67, 671, 673, 674, 678, 68, 688, 69, 694, 7, 70, 703, 704, 705, 707, 708, 709, 71, 710, 711, 714, 715, 718, 719, 72, 720, 722, 724, 725, 726, 728, 73, 731, 732, 733, 735, 736, 737, 74, 741, 743, 745, 748, 75, 750, 76, 761, 762, 765, 778, 780, 781, 782, 783, 784, 785, 786, 788, 79, 798, 799, 801, 802, 804, 805, 808, 809, 81, 810, 818, 82, 828, 829, 830, 832, 833, 835, 836, 837, 838, 839, 842, 848, 849, 85, 851, 852, 854, 858, 86, 860, 861, 862, 866, 867, 869, 870, 875, 876, 877, 879, 88, 880, 881, 882, 885, 886, 888, 89, 891, 892, 893, 896, 897, 898, 899, 900, 903, 904, 908, 910, 911, 912, 913, 914, 915, 916, 917, 918, 919, 92, 920, 921, 922, 923, 924, 926, 928, 93, 930, 931, 932, 933, 934, 936, 938, 94, 940, 942, 943, 944, 945, 946, 947, 948, 949, 95, 950, 951, 952, 953, 954, 955, 96, 968, 969, 97, 99, 994]
 
@@ -236,9 +235,12 @@ def save_data(mem, cpu, delay, hit_ratio, no):
     hit_ratio{host_no}_{no} = {hit_ratio}
     """
     send_email(data)
-    file = open(f'output{host_no}_{no}.py', 'w')
+    file = open(f'results/output{host_no}_{no}.py', 'w')
     file.write(data)
     file.close()
+    send_path = '/home/osboxes/results/'
+    sp.run(
+        ["scp", f'results/output{host_no}_{no}.py', f"osboxes@192.168.200.100:{send_path}"])
 
 
 def arrival_distribution():
@@ -249,7 +251,53 @@ def arrival_distribution():
     return (i for i in arrival_dist)
 
 
+class BrokerRequest:
+    def __init__(self, user, pw, ip, sub_topic):
+        self.user = user
+        self.pw = pw
+        self.ip = ip
+        self.port = 1883
+        self.topic = sub_topic
+        self.response = None
+        self.client = mqtt.Client()
+
+    def on_connect(self, connect_client, userdata, flags, rc):
+        print("Connected with Code :" + str(rc))
+        # Subscribe Topic from here
+        connect_client.subscribe(self.topic)
+
+    def on_message(self, message_client, userdata, msg):
+        if pickle.loads(msg.payload):
+            self.response = pickle.loads(msg.payload)
+
+    def broker_loop(self):
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        self.client.username_pw_set(self.user, self.pw)
+        self.client.connect(self.ip, self.port, 60)
+        self.client.loop_start()
+        while True:
+            if self.response:
+                self.client.loop_stop()
+                self.client.disconnect()
+                return self.response
+
+    def __del__(self):
+        print('BrokerRequest Object Deleted!')
+
+
+def initialization():
+    br = BrokerRequest(user='mec', pw='password', ip='192.168.200.100', sub_topic='control')
+    br.broker_loop()
+    del br
+    print('starting ....')
+
+
 def run(no_mec):
+    os.system('clear')
+    print('Waiting for Start command from Control...')
+    initialization()
     request_data = pd.read_csv(f'../request_data.csv')
     no_reqs = int(request_data.shape[0] * 0.3)  # testing data is 30 % => 67,259
     n = 150
