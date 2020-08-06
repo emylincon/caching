3000c1e16adc671dc62b99a4d6019af5c5554bf5
Emeka Ugwuanyi <emylincon@gmail.com>
2020-08-06 19:47:50 +0100

    build update

--- apline_build.sh
+++ apline_build.sh
@@ -3,7 +3,7 @@
 #  python:3.6-alpine
 apk update && apk upgrade
 apk add --no-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing hdf5 hdf5-dev
-apk --no-cache --update-cache add gcc gfortran python3-dev py-pip build-base wget freetype-dev libpng-dev openblas-dev
+apk --no-cache --update-cache add gcc gfortran python3 python3-dev py-pip build-base wget freetype-dev libpng-dev openblas-dev
 apk add jpeg-dev zlib-dev freetype-dev lcms2-dev openjpeg-dev tiff-dev tk-dev tcl-dev
 apk --update add libxml2-dev libxslt-dev libffi-dev gcc musl-dev libgcc openssl-dev curl
 ln -s /usr/include/locale.h /usr/include/xlocale.h
@@ -17,3 +17,4 @@ pip3 install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.13
 pip3 install scipy pandas
 pip3 install mlxtend
 pip3 install paramiko paho-mqtt requests
+apk add wget
